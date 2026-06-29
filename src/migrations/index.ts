3b172dfa6c9af434bf323a2f80d2b6eed9dac1bc
Anthony Pineda <anthony051413@gmail.com>
2026-06-29 13:43:09 -0500

    feat(migrations): add site_settings table migration for SiteSettings global

--- src/migrations/index.ts
+++ src/migrations/index.ts
@@ -1,9 +1,15 @@
 import * as migration_20260621_043659_initial_production_schema from './20260621_043659_initial_production_schema';
+import * as migration_20260629_184210_add_site_settings from './20260629_184210_add_site_settings';
 
 export const migrations = [
   {
     up: migration_20260621_043659_initial_production_schema.up,
     down: migration_20260621_043659_initial_production_schema.down,
-    name: '20260621_043659_initial_production_schema'
+    name: '20260621_043659_initial_production_schema',
+  },
+  {
+    up: migration_20260629_184210_add_site_settings.up,
+    down: migration_20260629_184210_add_site_settings.down,
+    name: '20260629_184210_add_site_settings'
   },
 ];
